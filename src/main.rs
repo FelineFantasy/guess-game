6f9f6c5ed158aee8bc61eef7d5f294bb4073fd12
FelineFantasy <mailsalavata5@gmail.com>
2026-07-27 20:10:19 +0500

    refactor: extract separator constants

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use ask_input::input;
 use rand::Rng;
 
+const SEPARATOR: &str = "=";
+const SEPARATOR_LEN: usize = 50;
+
 fn main() {
     let mut count = 0;
     let secret = rand::thread_rng().gen_range(1..=100);
 
     println!("Угадай число от 1 до 100!");
-    println!("{}", "=".repeat(50));
+    println!("{}", SEPARATOR.repeat(SEPARATOR_LEN));
 
     loop {
         print!("Введите число: ");
@@ -22,16 +25,16 @@ fn main() {
         count += 1;
 
         if guess == secret {
-            println!("{}", "=".repeat(50));
+            println!("{}", SEPARATOR.repeat(SEPARATOR_LEN));
             println!("Ты угадал за {} попыток!", count);
-            println!("{}", "=".repeat(50));
+            println!("{}", SEPARATOR.repeat(SEPARATOR_LEN));
             break;
         } else if guess < secret {
             println!("Больше {}!", guess);
         } else {
             println!("Меньше {}!", guess);
         }
-        println!("{}", "=".repeat(50));
+        println!("{}", SEPARATOR.repeat(SEPARATOR_LEN));
     }
 
     println!("Нажми Enter для выхода...");
